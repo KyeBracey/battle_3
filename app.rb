3ff1bb059776a47f84df663a473df4d09b77d1d3
Charlotte Fereday <cbrfereday@gmail.com>
2017-05-24 15:35:13 +0100

    Adds feature test for submitting player names & post request

--- app.rb
+++ app.rb
@@ -6,5 +6,11 @@ class Battle < Sinatra::Base
     erb(:index)
   end
 
+  post '/names' do
+    @player_one_name = params[:player_one_name]
+    @player_two_name = params[:player_two_name]
+    erb(:player_names)
+  end
+
   run! if app_file == $0
 end
--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -3,10 +3,9 @@ require_relative '../../app'
 RSpec.feature "Players can submit their names", :type => :feature do
   scenario "Player 1 can submit their name" do
     visit '/'
-    fill_in('Player One', :with => 'Charlotte')
-  end
-  scenario "Player 2 can submit their name" do
-    visit '/'
-    fill_in('Player Two', :with => 'Simon')
+    fill_in('player_one_name', :with => 'Charlotte')
+    fill_in('player_two_name', :with => 'Simon')
+    click_button 'Submit'
+    expect(page).to have_content 'Charlotte vs. Simon'
   end
 end
