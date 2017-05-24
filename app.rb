36c7183852f1bada5eec3775ad308fadde056d16
Simon Ashbery <si.ashbery@gmail.com>
2017-05-24 15:59:49 +0100

    Added a redirect to the names post method, which points to a get method for play

--- app.rb
+++ app.rb
@@ -1,15 +1,22 @@
 require 'sinatra/base'
 
+
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
-# comment 
   post '/names' do
-    @player_one_name = params[:player_one_name]
-    @player_two_name = params[:player_two_name]
+    session[:player_one_name] = params[:player_one_name]
+    session[:player_two_name] = params[:player_two_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_one_name = session[:player_one_name]
+    @player_two_name = session[:player_two_name]
     erb(:player_names)
   end
 
