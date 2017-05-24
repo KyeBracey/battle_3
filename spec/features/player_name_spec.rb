5efb3e020b3ecda6a85368c96952703ad8af7a0b
Charlotte Fereday <cbrfereday@gmail.com>
2017-05-24 16:27:20 +0100

    Adds web helpers file and updates player name spec

--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -2,20 +2,14 @@ require_relative '../../app'
 
 feature "Players can submit their names", :type => :feature do
   scenario "Player 1 can submit their name" do
-    visit '/'
-    fill_in('player_one_name', :with => 'Charlotte')
-    fill_in('player_two_name', :with => 'Simon')
-    click_button 'Submit'
+    sign_in_and_play()
     expect(page).to have_content 'Charlotte vs. Simon'
   end
 end
 
 feature "Players can see hit points", :type => :feature do
   scenario "Can see player hit points" do
-    visit '/'
-    fill_in('player_one_name', :with => 'Charlotte')
-    fill_in('player_two_name', :with => 'Simon')
-    click_button 'Submit'
+    sign_in_and_play()
     expect(page).to have_content 'Charlotte HP: 100'
     expect(page).to have_content 'Simon HP: 100'
   end
