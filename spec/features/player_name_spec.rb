1523d04b336e8e8e41a880852bab381a730a0640
Charlotte Fereday <cbrfereday@gmail.com>
2017-05-24 16:21:51 +0100

    Players can now see hit points: updates feature spec & player names

--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -1,11 +1,22 @@
 require_relative '../../app'
 
-RSpec.feature "Players can submit their names", :type => :feature do
+feature "Players can submit their names", :type => :feature do
   scenario "Player 1 can submit their name" do
     visit '/'
     fill_in('player_one_name', :with => 'Charlotte')
     fill_in('player_two_name', :with => 'Simon')
     click_button 'Submit'
     expect(page).to have_content 'Charlotte vs. Simon'
+  end
+end
+
+feature "Players can see hit points", :type => :feature do
+  scenario "Can see player hit points" do
+    visit '/'
+    fill_in('player_one_name', :with => 'Charlotte')
+    fill_in('player_two_name', :with => 'Simon')
+    click_button 'Submit'
+    expect(page).to have_content 'Charlotte HP: 100'
+    expect(page).to have_content 'Simon HP: 100'
   end
 end
