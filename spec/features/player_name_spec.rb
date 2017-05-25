05c21ba22638cc36224cfdad3585efc0a6a0e3a7
Kavita <kkavita92@gmail.com>
2017-05-25 12:46:35 +0100

    Replace Player 1 name in tests :)

--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -3,14 +3,14 @@ require_relative '../../app'
 feature "Players can submit their names", :type => :feature do
   scenario "Player 1 can submit their name" do
     sign_in_and_play()
-    expect(page).to have_content 'Charlotte vs. Simon'
+    expect(page).to have_content 'Kavita vs. Simon'
   end
 end
 
 feature "Players can see hit points", :type => :feature do
   scenario "Can see player hit points" do
     sign_in_and_play()
-    expect(page).to have_content 'Charlotte HP: 100'
+    expect(page).to have_content 'Kavita HP: 100'
     expect(page).to have_content 'Simon HP: 100'
   end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,6 @@
 def sign_in_and_play
   visit '/'
-  fill_in('player_one_name', :with => 'Charlotte')
+  fill_in('player_one_name', :with => 'Kavita')
   fill_in('player_two_name', :with => 'Simon')
   click_button 'Submit'
 end
