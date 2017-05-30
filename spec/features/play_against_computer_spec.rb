fe1f2aa51fa727f2a3555965b10667aff3015ff6
Kynosaur <kye.bracey@gmail.com>
2017-05-30 08:30:29 +0100

    Update previously committed test.  Update test suite to account for new starting menu. Break everything.

--- spec/features/play_against_computer_spec.rb
+++ spec/features/play_against_computer_spec.rb
@@ -1,11 +1,16 @@
 require_relative '../../app'
 
 feature 'Playing against a computerised opponent' do
-  scenario 'User clicks "Play against computer" button and plays a single player game' do
+  scenario 'User can start a game against the computer' do
+    visit('/')
+    fill_in('player_one_name', :with => 'Kye')
+  end
+
+  scenario 'User can play a game against the computer' do
     sign_in_and_play_against_computer
     click_button('ATTACK!')
     click_button('END TURN!')
-    expect(page).to have_content('Computer attacked you!')
+    expect(page).to have_content('Computer attacked you! Beep boop.')
   end
 
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,12 +1,14 @@
 def sign_in_and_play
   visit '/'
+  click_button('Play 2-player game')
   fill_in('player_one_name', :with => 'Kye')
   fill_in('player_two_name', :with => 'Simon')
-  click_button 'Submit'
+  click_button('Submit')
 end
 
 def sign_in_and_play_against_computer
   visit '/'
+  click_button('Play against computer')
   fill_in('player_one_name', :with => 'Kye')
-  click_button 'Play against computer'
+  click_button('Submit')
 end
