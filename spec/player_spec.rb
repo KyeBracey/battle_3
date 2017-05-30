6e7625c40926ae6aa19a365a0984aa3017a6d565
Kynosaur <kye.bracey@gmail.com>
2017-05-30 07:54:10 +0100

    Add in the default #receive_damage test I mistakenly removed

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -14,11 +14,16 @@ describe Player do
   end
 
   it 'players can override the default hit points and choose their own starting value' do
-    expect(player_with_50hp.hit_points).to eq 50
+    expect(player_with_50hp.hit_points).to eq(50)
   end
 
-  it 'players can lose hit points based on the argument passed in' do
-    expect{ player1.receive_damage(7) }.to change { player1.hit_points }.by(-7)
-  end
+  describe '#receive_damage' do
+    it 'players lose 10 hit points when no argument is passed in' do
+      expect{ player1.receive_damage }.to change { player1.hit_points }.by(-10)
+    end
 
+    it 'players can lose hit points based on the argument passed in' do
+      expect{ player1.receive_damage(7) }.to change { player1.hit_points }.by(-7)
+    end
+  end
 end
