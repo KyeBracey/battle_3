54c5f8c24bc05d0314516a3545829f288f81a5cd
Kynosaur <kye.bracey@gmail.com>
2017-05-30 08:18:26 +0100

    Add Computer#receive_damage and Computer::DEFAULT_DAMAGE

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,11 +1,16 @@
 class Computer
   DEFAULT_HIT_POINTS = 100
+  DEFAULT_DAMAGE = 10
 
   attr_reader :name, :hit_points
 
   def initialize(name = 'Computer', hit_points = DEFAULT_HIT_POINTS)
     @name = name
     @hit_points = hit_points
+  end
+
+  def receive_damage(amount = DEFAULT_DAMAGE)
+    @hit_points -= amount
   end
 
 end
