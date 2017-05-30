a3383cfcdd516445710e3066e157f7dbad21a19b
Kynosaur <kye.bracey@gmail.com>
2017-05-30 07:56:52 +0100

    Stub randomness for result screen feature tests so that they pass again

--- spec/features/result_screen_spec.rb
+++ spec/features/result_screen_spec.rb
@@ -1,8 +1,12 @@
-
 feature 'Result Screen' do
+
+  before(:each) do
+    srand(1337)
+  end
+
   scenario 'player 1 wins and sees a victory screen' do
     sign_in_and_play
-    19.times do
+    33.times do
       click_button('ATTACK!')
       click_button('END TURN!')
     end
@@ -10,7 +14,7 @@ feature 'Result Screen' do
   end
   scenario 'player 2 loses and has their face rubbed in it' do
     sign_in_and_play
-    19.times do
+    33.times do
       click_button('ATTACK!')
       click_button('END TURN!')
     end
