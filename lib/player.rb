0dd6a2d901cf7396e40d3c4b408c9d19c133429f
Kynosaur <kye.bracey@gmail.com>
2017-06-21 07:56:04 +0100

    Updates name entry forms so that all fields are required (and updates player name so it's not optional anymore in model

--- lib/player.rb
+++ lib/player.rb
@@ -3,7 +3,7 @@ class Player
   DEFAULT_DAMAGE = 10
   attr_reader :name, :hit_points
 
-  def initialize(name = "nil_player", hit_points = DEFAULT_HIT_POINTS)
+  def initialize(name, hit_points = DEFAULT_HIT_POINTS)
     @name = name
     @hit_points = hit_points
   end
